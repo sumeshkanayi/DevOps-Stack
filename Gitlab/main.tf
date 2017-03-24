ff0ad2eaa7255effe4db5edb85afa58f12506e77
sumeshkanayi <sumeshkanayi@gmail.com>
2017-03-24 16:09:25 +0530

    Update main.tf

--- Gitlab/main.tf
+++ Gitlab/main.tf
@@ -1,2 +1,13 @@
 #Chef recipe for installing Gitlab 
 #aws instance Ubuntu 16.04 t2.medium
+
+resource aws_instance 'gitlab'{
+  ami="ami-ee7bca8d"
+  instance_type = "t2.medium"
+  tags {
+    Name = "Gitlab"
+  }
+  security_groups=["sg-191c707e"]
+  key_name="siamol"
+  
+  }
